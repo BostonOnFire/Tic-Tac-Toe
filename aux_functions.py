6698d173d44d032c5a5cd46763090c262a8729d1
BostonOnFire <d.datsenko@outlook.com>
2024-02-27 05:15:36 +0100

    added board generation

---
+++ aux_functions.py
@@ -0,0 +1,5 @@
+def board_gen(coordinates):
+    board = (f"|{coordinates[1]}|{coordinates[2]}|{coordinates[3]}|\n"
+             f"|{coordinates[4]}|{coordinates[5]}|{coordinates[6]}|\n"
+             f"|{coordinates[7]}|{coordinates[8]}|{coordinates[9]}|")
+    print(board)
