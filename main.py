68999c1482652c4bc245fba91c86935b6eb0ed55
BostonOnFire <d.datsenko@outlook.com>
2024-02-28 17:39:42 +0100

    v0.3 added main menu and replay option

--- main.py
+++ main.py
@@ -5,36 +5,51 @@ coordinates = {1:'1', 2:'2', 3:'3', #Dictionary for navigating board coortinates
                4:'4', 5:'5', 6:'6',
                7:'7', 8:'8', 9:'9'}
 
-ongoing = True #Variable to track if the game still continues
-finished = False
-turn = 0 #Variable to determine whether it's even (X) or odd (O) turn
-turn_rep = -1 #Variable for error handling (turn to repeat)
-
-while ongoing:
-    os.system('cls' if os.name == 'nt' else 'clear') #Clears the screen
-    board_gen(coordinates) #Generates playing board
-    if turn_rep == turn: #Condition to handle an invalid input error
-        print("Podałeś niewłaściwe pole, spróbój ponownie")
-    turn_rep = turn
-    print("Tura gracza " + str((turn % 2) + 1) + ". Wybierz pole (1-9) albo wpisz 'w' żeby wyjść") #UI message with player's turn indicator
-    choice = input()
-    if choice == 'w': #Quits game if the input is 'w'
-        ongoing = False
-    elif str.isdigit(choice) and int(choice) in coordinates: #Checks if input is a valid coordinate
-        if not coordinates[int(choice)] in {'X', 'O'}: #Checks if selected field has already been taken by X or O
-            turn += 1
-            coordinates[int(choice)] = whose_turn(turn) #Fills selected field with current player's symbol (X or O)
-    if win_condition(coordinates):
-        ongoing, finished = False, True
-    if turn > 8:
-        ongoing = False
-
-os.system('cls' if os.name == 'nt' else 'clear')
-board_gen(coordinates)
-if finished:
-    if whose_turn(turn) == 'X':
-        print("Gracz 1 wygrywa! Gratulacje!")
+#Function containing core gameplay
+def gameplay():
+    ongoing = True #Variable to track if the game still continues
+    finished = False
+    turn = 0 #Variable to determine whether it's even (X) or odd (O) turn
+    turn_rep = -1 #Variable for error handling (turn to repeat)
+    coordinates.update({1:'1', 2:'2', 3:'3', #Board reset before game start
+                        4:'4', 5:'5', 6:'6',
+                        7:'7', 8:'8', 9:'9'})
+    while ongoing:
+        os.system('cls' if os.name == 'nt' else 'clear') #Clears the screen
+        print("Witaj w grze Kółko i Krzyżyk!")
+        board_gen(coordinates) #Generates playing board
+        if turn_rep == turn: #Condition to handle an invalid input error
+            print("Podałeś niewłaściwe pole, spróbój ponownie")
+        turn_rep = turn
+        print("Tura gracza " + str((turn % 2) + 1) + ". Wybierz pole (1-9) albo wpisz 'w' żeby wyjść") #UI message with player's turn indicator
+        choice = input()
+        if choice == 'w': #Quits game if the input is 'w'
+            ongoing = False
+        elif str.isdigit(choice) and int(choice) in coordinates: #Checks if input is a valid coordinate
+            if not coordinates[int(choice)] in {'X', 'O'}: #Checks if selected field has already been taken by X or O
+                turn += 1
+                coordinates[int(choice)] = whose_turn(turn) #Fills selected field with current player's symbol (X or O)
+        if win_condition(coordinates):
+            ongoing, finished = False, True
+        if turn > 8:
+            ongoing = False
+    os.system('cls' if os.name == 'nt' else 'clear') #Clears screen one last time
+    board_gen(coordinates) #Generates end board state
+    #Shows the winner or a draw if there's none
+    if finished:
+        if whose_turn(turn) == 'X':
+            print("Gracz 1 wygrywa! Gratulacje!")
+        else:
+            print("Gracz 2 wygrywa! Gratulacje!")
     else:
-        print("Gracz 2 wygrywa! Gratulacje!")
-else:
-    print("Remis")
+        print("Remis")
+
+#Main menu function
+def main():
+    while True:
+        gameplay()
+        replay = input("Chcesz zegrać jeszcze raz? y/n?") == 'y'
+        if not replay:
+            return
+        
+main()
